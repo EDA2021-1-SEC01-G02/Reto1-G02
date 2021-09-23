1c0af1123e76632f22c5e3863d6f4eee77ce864d
JuanPRodriguezB <jp.rodriguezb20@uniandes.edu.co>
2021-09-22 21:52:01 -0500

    Reto 1

--- App/controller.py
+++ App/controller.py
@@ -25,7 +25,6 @@ import config as cf
 from DISClib.ADT import list as lt
 import model
 import csv
-import time #TODO BORRAR
 
 
 """
@@ -48,13 +47,13 @@ def loadData(catalog, artists_info):
    
 
 def loadArtWorks(catalog):
-    artworksfile = cf.data_dir + 'MoMA/Artworks-utf8-small.csv'
+    artworksfile = cf.data_dir + 'MoMA/Artworks-utf8-10pct.csv'
     input_file = csv.DictReader(open(artworksfile, encoding = 'utf-8'))
     for artwork in input_file:
         model.addArtWork(catalog, artwork)
 
 def loadArtistsinfo(artists_info):
-    artistsfile = cf.data_dir + 'MoMA/Artists-utf8-small.csv'
+    artistsfile = cf.data_dir + 'MoMA/Artists-utf8-10pct.csv'
     input_file =  csv.DictReader(open(artistsfile, encoding= 'utf-8'))
     
     for info in input_file:
@@ -65,18 +64,10 @@ def loadArtistsinfo(artists_info):
 # Funciones de ordenamiento
 
 def sortArtistByDate(artists_info, year1, year2):
-    start_time = time.process_time()
-
     sortedArtists = (model.sortArtistByDate(artists_info))
     model.getYearRange(sortedArtists, year1, year2)
 
-    stop_time = time.process_time()
-    elapsed_time_mseg = (stop_time - start_time)*1000
-    print(elapsed_time_mseg)
-
 def sortByDate(artworks, date1, date2, artists_info):
-    start_time = time.process_time()
-
     model.sortByDate(artworks)
     range = lt.lastElement(artworks)['DateAcquired']
     while model.cmpDateAdquired(date1,range) == False :
@@ -95,27 +86,15 @@ def sortByDate(artworks, date1, date2, artists_info):
     artworksByDate = model.getArtworksByDate(artworks,date1,date2)
 
     model.getSixArtworks(artworksByDate, artists_info)    
-
-    stop_time = time.process_time()
-    elapsed_time_mseg = (stop_time - start_time)*1000
-    print(elapsed_time_mseg)
     
 
 # Funciones de consulta sobre el catálogo
 def artistsTecnique (artworks,artist_info,artist):
-    start_time = time.process_time()
-
     artistID = int(model.getArtistID(artist,artist_info))
     model.getArtworksByArtistsTechnique(artist,artistID,artworks)
-
-    stop_time = time.process_time()
-    elapsed_time_mseg = (stop_time - start_time)*1000
-    print(elapsed_time_mseg)
     
 
 def ObrasPorNacionalidad(artworks, artist_info):
-    start_time = time.process_time()
-
     consIDs =  model.newList('ARRAY_LIST', None)
     size = artworks['size'] + 1
 
@@ -124,17 +103,7 @@ def ObrasPorNacionalidad(artworks, artist_info):
     firstEl = model.sortNat(natList)
     model.getArtworksbyArtists(natList[firstEl][1], artworks, artist_info, firstEl)
 
-    stop_time = time.process_time()
-    elapsed_time_mseg = (stop_time - start_time)*1000
-    print(elapsed_time_mseg)
-
 def precioTransporte(artworks,artists,dep):
-    start_time = time.process_time()
-
     depArtworks = model.getDepArtworks(artworks,dep)
     prices = model.calculatePrice(depArtworks,artists)
     model.showPrice(prices[0],prices[1],prices[2],dep)
-
-    stop_time = time.process_time()
-    elapsed_time_mseg = (stop_time - start_time)*1000
-    print(elapsed_time_mseg)
